ea11b33283495d24efc9dbbe8ac7350048490276
user <bc5ae@herts.ac.uk>
2020-01-05 22:35:28 -0500

    Add function to upvotes and adding links to mongo

--- herbiz-app/routes/index.js
+++ herbiz-app/routes/index.js
@@ -52,9 +52,12 @@ router.param('comment', function(req, res, next, id) {
   })
 });
 
-router.get('/posts/:post', function(req, res) {
-  req.post.populate('comments', function(err,post){
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err,post) {
+    if (err) { return next(err); }
+
     res.json(req.post);
+      
   });  
 });
 
