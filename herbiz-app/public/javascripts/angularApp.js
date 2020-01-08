5f76e9659018fd79185f69ff129030d5999aee36
user <bc5ae@herts.ac.uk>
2020-01-08 10:58:32 -0500

    set up functions and auth for login and register

--- herbiz-app/public/javascripts/angularApp.js
+++ herbiz-app/public/javascripts/angularApp.js
@@ -30,6 +30,59 @@ function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('home');
   
 }])
+
+.factory('auth', ['$http', '$window', function($http, $window){
+  var auth = {};
+
+  auth.saveToken = function (token){
+    $window.localStorage['her-biz-token'] = token;
+  };
+  
+  auth.getToken = function (){
+    return $window.localStorage['her-biz-token'];
+  }
+
+  auth.isLoggedIn = function(){
+    var token = auth.getToken();
+  
+    if(token){
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+  
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+
+  auth.currentUser = function(){
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+  
+      return payload.username;
+    }
+  }
+
+  auth.register = function(user){
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  }
+
+  auth.logIn = function(user){
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function(){
+    $window.localStorage.removeItem('her-biz-token');
+  };
+
+ return auth;
+}])
+
+
 .factory('posts', ['$http',function($http){
   var o = {
     posts: []
@@ -115,5 +168,5 @@ function($scope, posts){
    $scope.incrementUpvotes = function(comment){
     posts.upvoteComment(post, comment);
   };
-  
+
   }]);
