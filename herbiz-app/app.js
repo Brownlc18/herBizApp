5fab39e570ab320ea888b558925fe4155b3bfebd
cBrown <bc5ae@herts.ac.uk>
2020-01-19 14:24:00 +0000

    mongoose error fix

--- herbiz-app/app.js
+++ herbiz-app/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
+mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost/news');
 
 require('./models/Posts');
